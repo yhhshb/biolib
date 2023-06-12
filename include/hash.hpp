3e531658c779689682d127e905f5163636fdcd68
yoshihiro shibuya <yoshihiro.shibuya@pasteur.fr>
2023-06-12 11:28:04 +0200

    added static methods to hash

--- include/hash.hpp
+++ include/hash.hpp
@@ -13,7 +13,7 @@ class double_hash64
     public:
         typedef uint64_t hash_type;
 
-        std::array<uint64_t, 2> operator()(uint8_t const* key, uint32_t keylen, uint32_t seed) const noexcept
+        static std::array<uint64_t, 2> hash(uint8_t const* key, uint32_t keylen, uint32_t seed) noexcept
         {
             std::array<uint64_t, 2> hval;
             MurmurHash3_x64_128(reinterpret_cast<const void*>(key), keylen, seed, hval.data());
@@ -21,9 +21,24 @@ class double_hash64
         }
 
         template <typename T>
+        static std::array<uint64_t, 2> hash(T val, uint64_t seed) noexcept
+        {
+            return hash(reinterpret_cast<uint8_t*>(&val), sizeof(T), seed);
+        }
+
+        std::array<uint64_t, 2> operator()(uint8_t const* key, uint32_t keylen, uint32_t seed) const noexcept
+        {
+            // std::array<uint64_t, 2> hval;
+            // MurmurHash3_x64_128(reinterpret_cast<const void*>(key), keylen, seed, hval.data());
+            // return hval;
+            return hash(key, keylen, seed);
+        }
+
+        template <typename T>
         std::array<uint64_t, 2> operator()(T val, uint64_t seed) const noexcept
         {
-            return operator()(reinterpret_cast<uint8_t*>(&val), sizeof(T), seed);
+            // return operator()(reinterpret_cast<uint8_t*>(&val), sizeof(T), seed);
+            return hash(val, seed);
         }
 };
 
@@ -32,19 +47,27 @@ class hash64
     public:
         typedef uint64_t hash_type;
 
+        uint64_t hash(uint8_t const* key, uint32_t keylen, uint32_t seed) const noexcept
+        {
+            return double_hash64::hash(key, keylen, seed)[0];
+        }
+
+        template <typename T>
+        uint64_t hash(T val, uint64_t seed) const noexcept
+        {
+            return double_hash64::hash(reinterpret_cast<uint8_t*>(&val), sizeof(T), seed);
+        }
+
         uint64_t operator()(uint8_t const* key, uint32_t keylen, uint32_t seed) const noexcept
         {
-            return hasher(key, keylen, seed)[0];
+            return hash(key, keylen, seed);
         }
 
         template <typename T>
         uint64_t operator()(T val, uint64_t seed) const noexcept
         {
-            return operator()(reinterpret_cast<uint8_t*>(&val), sizeof(T), seed);
+            return hash(reinterpret_cast<uint8_t*>(&val), sizeof(T), seed);
         }
-
-    private:
-        double_hash64 hasher;
 };
 
 class minimizer_position_extractor
