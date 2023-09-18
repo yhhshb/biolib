5640b685bba0ae7b4c3ebb5132c1883bf9c93da0
yoshihiro shibuya <yoshihiro.shibuya@pasteur.fr>
2023-09-18 14:04:22 +0200

    fixed bug in exception management when pushing inside the vector

--- include/bit_vector.hpp
+++ include/bit_vector.hpp
@@ -136,7 +136,8 @@ class vector
         std::size_t bsize;
 
         std::size_t bit_to_byte_size(std::size_t bit_size) const noexcept;
-        std::tuple<std::size_t, std::size_t> idx_to_coordinates(std::size_t idx) const;
+        std::tuple<std::size_t, std::size_t> idx_to_coordinates(std::size_t idx) const noexcept;
+        void check_coordinates(std::size_t idx) const;
 
         friend bool operator==(vector const& a, vector const& b) 
         {
@@ -201,6 +202,7 @@ vector<UnsignedIntegerType>::push_back(bool bit)
     auto [a, b] = idx_to_coordinates(bsize);
     if (a == _data.size()) _data.push_back(static_cast<block_type>(0));
     ++bsize;
+    check_coordinates(bsize);
     auto [block_idx, bit_idx] = idx_to_coordinates(bsize);
     _data[block_idx] |= static_cast<block_type>(bit) << (bit_idx);
 }
@@ -214,6 +216,7 @@ vector<UnsignedIntegerType>::push_back(UnsignedIntegerType block, std::size_t su
         _data.push_back(static_cast<block_type>(block >> (block_bit_size - b)));
     }
     bsize += suffix_len;
+    check_coordinates(bsize);
     auto [block_idx, bit_idx] = idx_to_coordinates(bsize);
     _data[block_idx] |= block << b;
 }
@@ -232,6 +235,7 @@ template <typename UnsignedIntegerType>
 void 
 vector<UnsignedIntegerType>::set(std::size_t idx)
 {
+    check_coordinates(idx);
     auto [block_idx, bit_idx] = idx_to_coordinates(idx);
     _data[block_idx] |= static_cast<UnsignedIntegerType>(1) << bit_idx;
 }
@@ -240,6 +244,7 @@ template <typename UnsignedIntegerType>
 void 
 vector<UnsignedIntegerType>::clear(std::size_t idx)
 {
+    check_coordinates(idx);
     auto [block_idx, bit_idx] = idx_to_coordinates(idx);
     _data[block_idx] &= ~(static_cast<UnsignedIntegerType>(1) << bit_idx);
 }
@@ -248,6 +253,7 @@ template <typename UnsignedIntegerType>
 bool 
 vector<UnsignedIntegerType>::at(std::size_t idx) const
 {
+    check_coordinates(idx);
     auto [block_idx, bit_idx] = idx_to_coordinates(idx);
     return static_cast<bool>(_data.at(block_idx) & (static_cast<UnsignedIntegerType>(1) << bit_idx));
 }
@@ -277,6 +283,7 @@ template <typename UnsignedIntegerType>
 UnsignedIntegerType&
 vector<UnsignedIntegerType>::operator[](std::size_t idx)
 {
+    check_coordinates(idx);
     auto [block_idx, bit_idx] = idx_to_coordinates(idx);
     return _data[block_idx];
 }
@@ -285,6 +292,7 @@ template <typename UnsignedIntegerType>
 UnsignedIntegerType const&
 vector<UnsignedIntegerType>::block_at(std::size_t idx) const
 {
+    check_coordinates(idx);
     auto [block_idx, bit_idx] = idx_to_coordinates(idx);
     return _data.at(block_idx);
 }
@@ -393,12 +401,18 @@ vector<UnsignedIntegerType>::bit_to_byte_size(std::size_t bit_size) const noexce
 
 template <typename UnsignedIntegerType>
 std::tuple<std::size_t, std::size_t> 
-vector<UnsignedIntegerType>::idx_to_coordinates(std::size_t idx) const
+vector<UnsignedIntegerType>::idx_to_coordinates(std::size_t idx) const noexcept
 {
-    if (idx >= bsize) throw std::out_of_range("[bit::vector] index to block coordinates");
     std::size_t block_idx = idx / block_bit_size;
     std::size_t bit_idx = idx % block_bit_size;
     return std::make_tuple(block_idx, bit_idx);
+}
+
+template <typename UnsignedIntegerType>
+void 
+vector<UnsignedIntegerType>::check_coordinates(std::size_t idx) const
+{
+    if (idx >= bsize) throw std::out_of_range("[bit::vector] index to block coordinates");
 }
 
 template <typename UnsignedIntegerType>
